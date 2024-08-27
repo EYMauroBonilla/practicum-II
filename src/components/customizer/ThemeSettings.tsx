585561630cbe3cda1fdd6889a56bba611e1d1a7f
MauroBonilla <Mauro.bonilla@mx.ey.com>
2024-08-27 09:57:30 -0600

    adding theme settings

--- src/components/customizer/ThemeSettings.tsx
+++ src/components/customizer/ThemeSettings.tsx
@@ -2,16 +2,27 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { BuildTheme } from '../../assets/global/Theme-varaible';
 
+interface CustomizerState {
+  activeDir: 'ltr' | 'rtl';
+  activeTheme: string;
+}
+
+interface RootState {
+  CustomizerReducer: CustomizerState;
+}
+
 const ThemeSettings = () => {
-  const customizer = useSelector((state) => state.CustomizerReducer);
+  const customizer = useSelector<RootState, CustomizerState>((state) => state.CustomizerReducer);
+
   const theme = BuildTheme({
     direction: customizer.activeDir,
-    theme: customizer.activeTheme,
   });
+
   useEffect(() => {
     document.dir = customizer.activeDir;
   }, [customizer]);
 
   return theme;
 };
+
 export default ThemeSettings;
